368ec9411e0b6c75b9e0dc75489c8bf11fa6c495
lucmsilva651 <lucmsilva651@gmail.com>
2024-06-09 19:00:26 -0300

    Added "on Relic" branding

---
+++ script.js
@@ -0,0 +1,15 @@
+const urlParams = new URLSearchParams(window.location.hash.substring(1));
+const searchTerm = urlParams.get('gsc.q');
+
+if (searchTerm) {
+  document.title = `"` + searchTerm + `"` + " on Relic";
+}
+
+var lastUrl = window.location.href;
+
+setInterval(function() {
+    if (window.location.href !== lastUrl) {
+        lastUrl = window.location.href;
+        location.reload();
+    }
+}, 1000);
